ab6f5dc7bc2fd5637f9ec75f3bf6aa16d89ad2c5
trinexometry <66057623+trinexometry@users.noreply.github.com>
2022-11-13 22:36:58 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,17 +5,6 @@ from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 from .database import Base
 
-
-"""class Post(Base):
-    __tablename__ = "posts"
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement='ignore_fk')
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
-    published = Column(Boolean, server_default= "True", nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default = text('now()'))
-"""
-
 class user(Base):
 
     __tablename__ = "users"
